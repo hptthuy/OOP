e4089cf4db49619ee8cf0e761d5741522496c7ff
hptthuy <145674106+hptthuy@users.noreply.github.com>
2024-03-04 14:31:37 +0700

    lab2

---
+++ lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static void bai2_1()
+            {
+                Time t1 = new Time();
+                Time t2 = new Time(3,2,7);
+                t1.Show();
+                t2.Show();
+            }
+
+        static void Main(string[] args)
+        {
+            bai2_1();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab2/Lab2/Lab2/Time.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Time
+    {
+        int hour;
+        int minute;
+        int second;
+        public int gethour { get { return hour; } }
+        public int SetHour (int i) { return hour = i; } 
+        int Minute { get; set; }
+        int Second { get; set; }
+
+        public Time() // hok có tham số thì khai báo ở trong ngoặc nhọn
+        {
+            hour = 0;
+            minute = 0;
+            second = 0;
+        }
+        public Time(int h, int m, int s) // có tham số thì khai báo trong ngoặc tròn 
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        }
+        public void Show()
+        {
+            Console.Write(" \n   " + hour + ":" + minute + ":" + second );
+
+
+        }
+
+    }
+}
+
